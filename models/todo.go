5d515a108c6edc25edee63ac696a2c9293422909
Kota Nonaka <kota.kaicho@gmail.com>
2018-05-23 23:52:37 +0900

    add db provider.

--- models/todo.go
+++ models/todo.go
@@ -1,9 +1,17 @@
 package models
 
 import (
+	"time"
 )
 
+// Todo model
 type Todo struct {
-	ID int
-	
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	IsDone    bool      `json:"isDone"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
+
+// Todos model
+type Todos []Todo
--- server.go
+++ server.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 
-	"github.com/uutarou10/todo-app/handlers"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/uutarou10/todo-app/handlers"
 )
 
 // App global values
@@ -12,6 +15,13 @@ type App struct {
 	Echo *echo.Echo
 	Host string
 	Port int
+	DB   *sql.DB
+}
+
+// CustomContext extends echo.Context
+type CustomContext struct {
+	echo.Context
+	DB *sql.DB
 }
 
 // NewApp creates App instance
@@ -26,12 +36,33 @@ func NewApp(host string, port int) *App {
 // Run calls start server
 func (a *App) Run() {
 	a.RegisterRoutes()
+	a.RegisterMiddlewares()
 	a.Echo.Start(fmt.Sprintf("%s:%d", a.Host, a.Port))
 }
 
 // RegisterRoutes registration endpoints.
 func (a *App) RegisterRoutes() {
 	a.Echo.GET("/", handlers.HelloHandler)
+}
+
+// RegisterMiddlewares registrationg middlewares.
+func (a *App) RegisterMiddlewares() {
+	// DB provider
+	a.Echo.Use(func(h echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			db, err := sql.Open("sqlite3", "db.sqlite3")
+			if err != nil {
+				panic("Cannot establish db connection.")
+			}
+			cc := &CustomContext{
+				c,
+				db,
+			}
+
+			return h(cc)
+		}
+	})
+	a.Echo.Use(middleware.Logger())
 }
 
 func main() {
