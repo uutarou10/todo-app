22ce6c75a2218587a6f7cc785b0623429e9d31db
Kota Nonaka <kota.kaicho@gmail.com>
2018-05-24 11:30:50 +0900

    add docker-compose

--- models/todo.go
+++ models/todo.go
@@ -8,9 +8,19 @@ import (
 type Todo struct {
 	ID        int       `json:"id"`
 	Title     string    `json:"title"`
+	Description string `json:"description"`
 	IsDone    bool      `json:"isDone"`
+	ProjectID int       `json:"project_id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func NewTodo(title string, isDone bool, projectId int) *Todo {
+	return &Todo{
+		Title:     title,
+		IsDone:    isDone,
+		ProjectID: projectId,
+	}
 }
 
 // Todos model
